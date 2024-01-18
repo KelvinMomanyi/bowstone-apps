96e0575f1905d7ff94e2e6affc537a547020271a
KelvinMomanyi <kelvinmata42@gmail.com>
2024-01-18 15:30:27 +0300

    new

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -8,19 +8,22 @@ const Whatsapp = () => {
 
     const handleDivClick = () => {
       // Find the anchor element by its ID or another selector
-      const anchorElement = document.getElementById('yourAnchorId'); // Replace with your actual anchor ID
+      const anchorElement = document.getElementById('whatsappLink'); // Replace with your actual anchor ID
   
       // Trigger a click on the anchor element
       if (anchorElement) {
         anchorElement.click();
       }
     };
+
+  
+      
     
   return (
-   <div  className=' fixed bottom-4 right-4  bg-green-500 rounded-full flex z-20 p-3 gap-4' onClick={handleDivClick} >
+   <div href={whatsappLink} className=' fixed bottom-4 right-4  bg-green-500 rounded-full flex z-20 p-3 gap-4' onClick={handleDivClick} >
 
-    <FaWhatsapp color='white' size={40} />
-    <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className='flex flex-col text-white hidden sm:block'>
+    <FaWhatsapp color='white' size={40}/>
+    <a id="whatsappLink" href={whatsappLink} target="_blank" rel="noopener noreferrer" className='flex flex-col z-10 text-white hidden sm:block'>
       <div className='flex flex-col font-secondary'>
         <span className='text-sm font-semibold'>Online Team</span>
         <span className='text-md'>Need Help, Chat with Us</span>
